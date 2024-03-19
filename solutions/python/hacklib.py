322fe5d8e433752abbf1680193a42292a2660e9c
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2024-03-19 10:23:15 -0400

    Implementation of list_primality function

--- solutions/python/hacklib.py
+++ solutions/python/hacklib.py
@@ -1,5 +1,15 @@
-import math
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 19 04:43:40 2024
 
+@author: touaf
+"""
+
+import math, array
+from typing import List, TypeVar, cast, Optional, Protocol, Generator, Any, Callable, Dict, Generic
+
+# verifie la primalite d'un nombre
 def is_prime(x: int) -> bool:
 	if x <= 1:
 		return False
@@ -12,3 +22,42 @@ def is_prime(x: int) -> bool:
 			if x % i == 0:
 				return False
 		return True
+
+# renvoie une liste de booleen 
+# pour 0 <= i <= n, result[i] == True si n premier, False sinon
+def list_primality(n: int) -> List[bool]:
+	result: List[bool] = [True] * (n + 1)
+	result[0] = result[1] = False
+	for i in range(math.isqrt(n) + 1):
+		if result[i]:
+			for j in range(i * i, len(result), i):
+				result[j] = False
+	return result
+
+
+E = TypeVar("E", bound="_Comparable")
+
+
+class _Comparable(Protocol):
+	def __lt__(self: E, other: E) -> bool: ...
+	def __le__(self: E, other: E) -> bool: ...
+	def __gt__(self: E, other: E) -> bool: ...
+	def __ge__(self: E, other: E) -> bool: ...
+
+def next_permutation(arr: List[E]) -> bool:
+	# Find non-increasing suffix
+	i: int = len(arr) - 1
+	while i > 0 and arr[i - 1] >= arr[i]:
+		i -= 1
+	if i <= 0:
+		return False
+	
+	# Find successor to pivot
+	j: int = len(arr) - 1
+	while arr[j] <= arr[i - 1]:
+		j -= 1
+	arr[i - 1], arr[j] = arr[j], arr[i - 1]
+	
+	# Reverse suffix
+	arr[i : ] = arr[len(arr) - 1 : i - 1 : -1]
+	return True
